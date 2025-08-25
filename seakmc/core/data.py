e378b7e596377330b4991fdb117a627208b31185
Tao Liang <xhtliang120@gmail.com>
2025-08-25 01:07:37 -0400

    Add files via upload

--- seakmc/core/data.py
+++ seakmc/core/data.py
@@ -2227,6 +2227,16 @@ class ActiveVolume(SeakmcData, MSONable):
     def __repr__(self):
         return self.__str__()
 
+    def Write_AV(self, filename, Fixed=False):
+        newAV = copy.deepcopy(self)
+        if not Fixed:
+            inds = np.arange(newAV.nactive, dtype=int)
+            newAV.atoms = newAV.atoms.iloc[inds]
+            newAV.natoms = len(newAV.atoms)
+            newAV.write_file(filename)
+        else:
+            newAV.write_file(filename)
+
     def to_coords(self, Buffer=False, Fixed=False):
         nout = self.nactive
         if Buffer: nout += self.nbuffer
